a86203ebb1b1762ddf503e7ad8547e3cf5b0e1c1
taotao <1985504469@qq.com>
2023-03-28 15:25:45 +0800

    props 练习

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {x: 1}
+  }
+
+  onClick = () => {
+    this.setState({
+      x: this.state.x + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        App
+        <button onClick={this.onClick}>+1</button>
+        <B name={this.state.x}/>
+      </div>
+    )
+  }
+}
+
+class B extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
 }
 
 export default App
