4af6a2150861cc8ff2594a756359ae3522e26139
taotao <1985504469@qq.com>
2023-03-31 22:25:48 +0800

    react 模拟生命周期

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import {React, useState} from 'react'
-
-// class App extends React.PureComponent {
-//   constructor(props) {
-//     super(props)
-//     this.state = {n: 1}
-//   }
-//
-//   onClick = () => {
-//     this.setState(state => ({n: state.n + 1}))
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         {this.state.n}
-//         <button onClick={this.onClick}>+1</button>
-//       </div>
-//     )
-//   }
-// }
+import React, {useState, useEffect} from 'react'
 
 const App = props => {
   const [n, setN] = useState(0)
   const onClick = () => {
     setN(n + 1)
   }
+
+  useEffect(() => {
+    console.log('只要变了我就执行')
+  })
+
+  useEffect(() => {
+    console.log('我只执行一次')
+  }, [])
+
+  useEffect(() => {
+    console.log('n变了我就执行')
+  }, [n])
+
   return (
     <div>
       {n}
