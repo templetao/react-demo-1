03a0a3bff5865c37923b53e3503e7ecc8be1ee12
taotao <1985504469@qq.com>
2023-03-31 22:46:51 +0800

    自定义 hook useUpdate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, {useState, useEffect} from 'react'
 
+const useUpdate = (fn, dep) => {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    setCount(x => x + 1)
+  }, [dep])
+
+  useEffect(() => {
+    if (count > 1) {
+      fn()
+    }
+  }, [count])
+}
+
 const App = props => {
   const [n, setN] = useState(0)
   const onClick = () => {
     setN(n + 1)
   }
 
-  useEffect(() => {
-    console.log('只要变了我就执行')
-  })
-
-  useEffect(() => {
-    console.log('我只执行一次')
-  }, [])
-
-  useEffect(() => {
-    console.log('n变了我就执行')
-  }, [n])
+  useUpdate(() => {
+    console.log('n变了我就执行，但是第一次不执行')
+  }, n)
 
   return (
     <div>
